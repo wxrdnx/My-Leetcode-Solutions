0bbd7ebdaef145adc910aab57ae0e84bd543f46e
wxrdnx <wxrdnx@protonmail.com>
2022-12-19 22:04:52 +0800

    Add 16

---
+++ 3sum-closest.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int threeSumClosest(vector<int>& nums, int target) {
+        int size = nums.size();
+        int min_diff = numeric_limits<int>::max();
+        int result;
+        sort(nums.begin(), nums.end());
+        for (int curr = 0; curr < size - 2; curr++) {  
+            int left = curr + 1, right = size - 1;
+            while (left < right) {
+                int sum = nums[curr] + nums[left] + nums[right];
+                int diff = abs(sum - target);
+                if (diff == 0) {
+                    return target;
+                } else if (diff < min_diff) {
+                    min_diff = diff;
+                    result = sum;
+                }
+                if (sum > target) {
+                    right--;
+                } else {
+                    left++;
+                }
+            }
+        }
+        return result;
+    }
+};
+
+// Time: O(n^2)
+// Space: O(1)
