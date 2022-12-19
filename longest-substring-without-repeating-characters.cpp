02200393de92af8a068b28e4e2f174811f5d99f4
wxrdnx <wxrdnx@protonmail.com>
2022-12-19 23:07:23 +0800

    Add 3

---
+++ longest-substring-without-repeating-characters.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        vector<int> indices(0x100, -1);
+        int longest = 0;
+        int left = 0, right = 0;
+        while (right < s.length()) {
+            if (indices[s[right]] >= left) {
+                longest = max(right - left, longest);
+                left = indices[s[right]] + 1;
+            }
+            indices[s[right]] = right;
+            right++;
+        }
+        return max(longest, right - left);
+    }
+};
+
+// Time: O(n)
+// Space: O(1)
