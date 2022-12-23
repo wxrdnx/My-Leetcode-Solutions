c8ca3e2b9e0727aa8d2ae640b0b239f463470204
wxrdnx <wxrdnx@protonmail.com>
2022-12-23 18:32:53 +0800

    Add 704

---
+++ binary-search.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int left = 0, right = nums.size() - 1, mid;
+        while (left <= right) {
+            mid = left + (right - left) / 2;
+            if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return -1;
+    }
+};
+
+// Time: O(log(n))
+// Space: O(1)
