290a4ff95fac26f4b50eb9251864aa0d28a979f5
wxrdnx <wxrdnx@protonmail.com>
2022-12-23 17:38:03 +0800

    Refactor 239

--- sliding-window-maximum_2.cpp
+++ sliding-window-maximum_2.cpp
@@ -3,21 +3,11 @@ public:
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
         vector<int> left_max(nums.size()), right_max(nums.size());
         vector<int> result;
-        for (int i = 0, curr_max; i < nums.size(); i++) {
-            if (i % k == 0) {
-                curr_max = nums[i];
-            } else if (nums[i] > curr_max) {
-                curr_max = nums[i]; 
-            }
-            left_max[i] = curr_max;
+        for (int i = 0; i < nums.size(); i++) {
+            left_max[i] = (i % k == 0 ? nums[i] : max(left_max[i - 1], nums[i]));
         }
         for (int i = nums.size() - 1, curr_max; i >= 0; i--) {
-            if (i == nums.size() - 1 || (i + 1) % k == 0) {
-                curr_max = nums[i];
-            } else if (nums[i] > curr_max) {
-                curr_max = nums[i]; 
-            }
-            right_max[i] = curr_max;
+            right_max[i] = ((i == nums.size() - 1 || (i + 1) % k == 0) ? nums[i] : max(right_max[i + 1], nums[i]));
         }
         for (int i = k - 1; i < nums.size(); i++) {
             result.push_back(max(left_max[i], right_max[i - k + 1]));
