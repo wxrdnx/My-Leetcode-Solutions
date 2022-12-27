de6d0ad731b238aadf8e9526f0e3522d3c8d5e2d
wxrdnx <wxrdnx@protonmail.com>
2022-12-27 21:24:57 +0800

    Add 130

---
+++ surrounded-regions.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        int m = board.size(), n = board[0].size();
+        for (int i = 0; i < m; i++) {
+            dfs(board, i, 0, m, n);
+            dfs(board, i, n - 1, m, n);
+        }
+        for (int j = 0; j < n; j++) {
+            dfs(board, 0, j, m, n);
+            dfs(board, m - 1, j, m, n);
+        }
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == 'V') {
+                    board[i][j] = 'O';
+                } else if (board[i][j] == 'O') {
+                    board[i][j] = 'X';
+                }
+            }
+        }
+    }
+private:
+    void dfs(vector<vector<char>>& board, int i, int j, int m, int n) {
+        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') {
+            return;
+        }
+        board[i][j] = 'V';
+        dfs(board, i + 1, j, m, n);
+        dfs(board, i - 1, j, m, n);
+        dfs(board, i, j + 1, m, n);
+        dfs(board, i, j - 1, m, n);
+    }
+};
