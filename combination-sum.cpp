03c074cd533a10a6aa2b43a52bc8870fd15d59a6
wxrdnx <wxrdnx@protonmail.com>
2022-12-28 14:30:48 +0800

    Add 39

---
+++ combination-sum.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        sort(candidates.begin(), candidates.end());
+        vector<vector<int>> result;
+        backtrack(result, candidates, {}, 0, target);
+        return result;
+    }
+private:
+    void backtrack(vector<vector<int>>& result, vector<int>& candidates, vector<int>&& nums, int curr, int total) {
+        if (total == 0) {
+            result.push_back(nums);
+            return;
+        }
+        for (int i = curr; i < candidates.size() && candidates[i] <= total; i++) {
+            nums.push_back(candidates[i]);
+            backtrack(result, candidates, move(nums), i, total - candidates[i]);
+            nums.pop_back();
+        }
+    }
+};
