685b7ba9d62b352f1641ac482841c417a46c7252
wxrdnx <wxrdnx@protonmail.com>
2022-12-28 15:33:10 +0800

    Redo 424

--- longest-repeating-character-replacement.cpp
+++ longest-repeating-character-replacement.cpp
@@ -1,21 +1,20 @@
 class Solution {
 public:
     int characterReplacement(string s, int k) {
-        vector<int> count(26);
+        vector<int> mapping(256);
         int left = 0, right = 0;
-        int result = 0, max_count = 0, curr_count = 0;
+        int result = 0, max_len = 0;
         while (right < s.length()) {
-            curr_count = ++count[s[right] - 'A'];
-            right++;
-            max_count = max(max_count, curr_count);
-            while (right - left - max_count > k) {
-                curr_count = --count[s[left] - 'A'];
+            mapping[s[right]]++;
+            max_len = max(max_len, mapping[s[right]]);
+            while (right - left + 1 - max_len > k) {
+                mapping[s[left]]--;
                 left++;
-                max_count = max(max_count, curr_count);
             }
-            result = max(result, right - left);
+            result = max(result, right - left + 1);
+            right++;
         }
-        return max(result, right - left);
+        return result;
     }
 };
 
