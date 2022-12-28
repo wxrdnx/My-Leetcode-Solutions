2be4fc3e82ee1963560a18b6a9ce95923f6f6fd4
wxrdnx <wxrdnx@protonmail.com>
2022-12-28 15:40:36 +0800

    Add 50

---
+++ powx-n.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n == -1) {
+            return 1 / x;
+        }
+        return n % 2 == 0 ? myPow(x * x, n / 2) : x * myPow(x, n - 1);
+    }
+};
+
+// Time: O(log(n))
