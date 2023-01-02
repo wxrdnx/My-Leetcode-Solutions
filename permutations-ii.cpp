ee15b947f577cce98e8c04f98009b6cab20e1119
wxrdnx <wxrdnx@protonmail.com>
2023-01-02 14:13:50 +0800

    change helper to backtrack

--- permutations-ii.cpp
+++ permutations-ii.cpp
@@ -3,11 +3,11 @@ public:
     vector<vector<int>> permuteUnique(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         vector<vector<int>> result;
-        permuteUniqueHelper(result, nums, 0);
+        backtrack(result, nums, 0);
         return result;
     }
 private:
-    void permuteUniqueHelper(vector<vector<int>>& result, vector<int>& nums, int now) {
+    void backtrack(vector<vector<int>>& result, vector<int>& nums, int now) {
         if (now == nums.size()) {
             result.push_back(nums);
             return;
@@ -15,7 +15,7 @@ private:
         for (int i = now; i < nums.size(); i++) {
             if (i == now || nums[i] != nums[now]) {
                 swap(nums[i], nums[now]);
-                permuteUniqueHelper(result, nums, now + 1);
+                backtrack(result, nums, now + 1);
             }
         }
         rotate(nums.begin() + now, nums.begin() + now + 1, nums.end());
