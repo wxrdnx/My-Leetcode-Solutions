72dfd47a4510f97b547e67c194f05d5b4065d2bd
wxrdnx <wxrdnx@protonmail.com>
2023-01-08 11:45:42 +0800

    change m, n to board.size(), board[0].size()

--- surrounded-regions.cpp
+++ surrounded-regions.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     void solve(vector<vector<char>>& board) {
-        int m = board.size(), n = board[0].size();
-        for (int i = 0; i < m; i++) {
-            dfs(board, i, 0, m, n);
-            dfs(board, i, n - 1, m, n);
+        for (int i = 0; i < board.size(); i++) {
+            dfs(board, i, 0);
+            dfs(board, i, board[0].size() - 1);
         }
-        for (int j = 0; j < n; j++) {
-            dfs(board, 0, j, m, n);
-            dfs(board, m - 1, j, m, n);
+        for (int j = 0; j < board[0].size(); j++) {
+            dfs(board, 0, j);
+            dfs(board, board.size() - 1, j);
         }
-        for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[0].size(); j++) {
                 if (board[i][j] == 'V') {
                     board[i][j] = 'O';
                 } else if (board[i][j] == 'O') {
@@ -21,14 +20,14 @@ public:
         }
     }
 private:
-    void dfs(vector<vector<char>>& board, int i, int j, int m, int n) {
-        if (i < 0 || i >= m || j < 0 || j >= n || board[i][j] != 'O') {
+    void dfs(vector<vector<char>>& board, int i, int j) {
+        if (i < 0 || i >= board.size() || j < 0 || j >= board[0].size() || board[i][j] != 'O') {
             return;
         }
         board[i][j] = 'V';
-        dfs(board, i + 1, j, m, n);
-        dfs(board, i - 1, j, m, n);
-        dfs(board, i, j + 1, m, n);
-        dfs(board, i, j - 1, m, n);
+        dfs(board, i + 1, j);
+        dfs(board, i - 1, j);
+        dfs(board, i, j + 1);
+        dfs(board, i, j - 1);
     }
 };
