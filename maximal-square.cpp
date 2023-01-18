ee58ebada77d2e5dc062dfaa03e77e163e78107d
wxrdnx <wxrdnx@protonmail.com>
2023-01-18 12:00:34 +0800

    Optimize maximal square

--- maximal-square.cpp
+++ maximal-square.cpp
@@ -1,22 +1,23 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int m = matrix.size(), n = matrix[0].size();
+        vector<int> dp(matrix[0].size() + 1);
         int max_len = 0;
         /* 
          * matrix[i][j] == 0 -> dp[i][j] = 0
          * matrix[i][j] == 1 -> dp[i][j] = min(dp[i][j - 1], dp[i - 1][j], dp[i - 1][j - 1]) + 1
          */
-        vector<vector<int>> dp(2, vector<int>(n + 1));
-        for (int i = 1; i <= m; i++) {
-            int p = i & 1, q = p ^ 1;
-            for (int j = 1; j <= n; j++) {
+        for (int i = 1; i <= matrix.size(); i++) {
+            int r1c1 = 0;
+            for (int j = 1; j <= matrix[0].size(); j++) {
+                int tmp = dp[j];
                 if (matrix[i - 1][j - 1] == '0') {
-                    dp[p][j] = 0;
+                    dp[j] = 0;
                 } else {
-                    dp[p][j] = min({dp[p][j - 1], dp[q][j], dp[q][j - 1]}) + 1;
-                    max_len = max(max_len, dp[p][j]);
+                    dp[j] = min({dp[j - 1], dp[j], r1c1}) + 1;
+                    max_len = max(max_len, dp[j]);
                 }
+                r1c1 = tmp;
             }
         }
         return max_len * max_len;
