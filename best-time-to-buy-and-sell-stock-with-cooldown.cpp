f693a1a9fad2a1b96ea48bf48c30e3458ff405ab
wxrdnx <wxrdnx@protonmail.com>
2023-01-25 21:32:39 +0800

    add dp equation

--- best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -1,6 +1,11 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+        /*
+         * buy[i] = max(buy[i - 1], cooldown[i - 1] - price[i])
+         * sell[i] = buy[i - 1] + price[i]
+         * cooldown[i] = mqx(cooldown[i - 1], sell[i - 1])
+         */
         int buy = numeric_limits<int>::min(), sell = 0, cooldown = 0;
         for (auto& price : prices) {
             int sell_tmp = sell;
