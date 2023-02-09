254c436530355d2458861b40afb30a29b26f40d2
wxrdnx <wxrdnx@protonmail.com>
2023-02-09 11:46:55 +0800

    Add 137

---
+++ single-number-ii.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int one = 0, two = 0;
+        for (int num : nums) {
+            one = (one ^ num) & ~two;
+            two = (two ^ num) & ~one;
+        }
+        return one;
+    }
+};
