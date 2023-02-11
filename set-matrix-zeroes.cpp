de98aee5e590cff4f58e4551f5346fd2cd2020f2
wxrdnx <wxrdnx@protonmail.com>
2023-02-11 15:37:05 +0800

    add O(m + n) solution

--- set-matrix-zeroes.cpp
+++ set-matrix-zeroes.cpp
@@ -1,6 +1,30 @@
 class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
+        vector<bool> rows(matrix.size()), cols(matrix[0].size());
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (matrix[i][j] == 0) {
+                    rows[i] = cols[j] = true;
+                }
+            }
+        }
+        for (int i = 0; i < matrix.size(); i++) {
+            for (int j = 0; j < matrix[0].size(); j++) {
+                if (rows[i] || cols[j]) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+    }
+};
+
+// Time: O(m * n)
+// Space: O(m + n)
+
+class Solution {
+public:
+    void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size(), n = matrix[0].size();
         bool set_col = false;
         for (int i = 0; i < m; i++) {
